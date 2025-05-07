b9ac0a2d22566615b4484610b83a4898efce8547
Mohammed Ali <binbasri.india@gmail.com>
2025-05-07 11:36:49 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_HF_API_URL, // must be https!
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // must be https!
   timeout: 5000,
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_HF_API_TOKEN}`,
     "Content-Type": "application/json",
   },
 });

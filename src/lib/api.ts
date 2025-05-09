300762b88a17939083d971ff84eced928e3bf0ea
Binbasri-in <mohammed.binbasri@gmail.com>
2025-05-09 07:00:50 +0530

    back to huggingface

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // must be https!
+  baseURL: process.env.NEXT_PUBLIC_HF_API_URL, // must be https!
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_HF_API_KEY}`,
+  },
 });
 
 export default api;
